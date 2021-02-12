1d69df331835b51af00cf024e89939c480b84bb3
川田京助 <kkyopa@gmail.com>
2021-02-12 15:25:42 +0900

    入力画面途中(わからん)

--- src/pages/first_step/CustomerInput.tsx
+++ src/pages/first_step/CustomerInput.tsx
@@ -1,22 +1,48 @@
 import React, { ReactElement } from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { Button, Grid, Box } from "@material-ui/core";
+import { HumanDataInput } from "../../components/block/RegisterInput";
 import "../../styles/App.css";
+import { UserContext } from "../App";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    gridCenter: {
+      justify: "center",
+      alignItems: "center",
+      spacing: 1,
+    },
+    HumanBox: {
+      marginBottom: "20px",
+    },
+  })
+);
 
 export default function CustomerInput(
   props: RouteComponentProps
 ): ReactElement {
+  const classes = useStyles();
+  const { TestHuman: TestHuman, setTestHuman: setTestHuman } = React.useContext(
+    UserContext
+  );
   const Next = () => {
     props.history.push({ pathname: "/customer_confime" });
   };
+
+  // const HumansInput = () => {
+  //   <HumanDataInput
+  //     name={"むず"}
+  //     onNameChanged={(name) => {
+  //       onNameChanged(count, name);
+  //     }}
+  //   />;
+  // };
+
   return (
     <div>
-      <h2>ユーザー登録画面</h2>
-      {/* <button>戻る</button> */}
+      <Box className={classes.HumanBox}>ユーザー登録画面</Box>
       <button onClick={Next}>次へ</button>
     </div>
   );
 }
-
-// const customerinput = () => {
-//   return { CustomerInput };
-// };
